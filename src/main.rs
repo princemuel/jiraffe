0a5192fe1f643082db7ae2ec2f513762542036ed
Prince Muel <vansomecsam@gmail.com>
2024-06-27 07:45:46 +0100

    feat: add test suite
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
+use anyhow::{self, Context};
 use clap::Parser;
-use std::{fs, path};
+use std::{self, fs, io, path};
 
 #[derive(Parser)]
 struct Cli {
-    pattern: String,
-    path: path::PathBuf,
+  pattern: String,
+  path: path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::parse();
+fn main() -> anyhow::Result<()> {
+  let args = Cli::parse();
 
-    let content = fs::read_to_string(&args.path).expect("could not read file");
+  let result = fs::read_to_string(&args.path);
+  let content = result.with_context(|| {
+    format!("could not read file `{}`", args.path.display())
+  })?;
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
+  println!("file content: {}", content);
+
+  find_matches(&content, &args.pattern, &mut io::stdout());
+
+  Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl io::Write) {
+  for line in content.lines() {
+    if line.contains(pattern) {
+      writeln!(writer, "{}", line);
     }
+  }
+}
+
+#[test]
+fn find_a_match() {
+  let mut result = Vec::new();
+  find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+  assert_eq!(result, b"lorem ipsum\n");
 }
