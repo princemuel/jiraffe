86a4f3af4e187b8c691cecc1ba5937c9b0d75085
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-07-11 19:25:37 +0100

    feat(db): model the db representation in Rust

--- src/models.rs
+++ src/models.rs
@@ -6,26 +6,32 @@ pub enum Status {
 }
 
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
+    pub name:        String,
+    pub description: String,
+    pub status:      Status,
+    pub stories:     Vec<u32>,
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        Self { name, description, status: Status::Open, stories: Vec::with_capacity(8) }
     }
 }
 
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    pub name:        String,
+    pub description: String,
+    pub status:      Status,
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        Self { name, description, status: Status::Open }
     }
 }
 
 pub struct DBState {
-    // This struct represents the entire db state which includes the last_item_id, epics, and
-    // stories TODO: add fields (make sure the fields are public)
+    pub last_item_id: u32,
+    pub epics:        Vec<Epic>,
+    pub stories:      Vec<Story>,
 }
